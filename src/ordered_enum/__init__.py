89eb2b1f858fe1c59f371063fb706db8d07860ab
sasa cocic <cocicsasa@gmail.com>
2024-05-27 09:39:57 -0400

    change exports to be public

--- src/ordered_enum/__init__.py
+++ src/ordered_enum/__init__.py
@@ -1,3 +1,4 @@
-from ordered_enum.ordered_enum import OrderedEnum, ValueOrderedEnum  # noqa: F401
+from ordered_enum.ordered_enum import OrderedEnum as OrderedEnum
+from ordered_enum.ordered_enum import ValueOrderedEnum as ValueOrderedEnum
 
 __version__ = "0.0.8"
