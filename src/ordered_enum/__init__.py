e079e1f7530d264fb03b49f602c04192f003f320
sasa cocic <cocicsasa@gmail.com>
2024-06-05 17:18:48 -0400

    move __all__ to end

--- src/ordered_enum/__init__.py
+++ src/ordered_enum/__init__.py
@@ -1,10 +1,10 @@
+from ordered_enum.ordered_enum import OrderedEnum
+from ordered_enum.ordered_enum import ValueOrderedEnum
+
+__version__ = "0.0.8"
+
 __all__ = [
     "__version__",
     "OrderedEnum",
     "ValueOrderedEnum",
 ]
-
-from ordered_enum.ordered_enum import OrderedEnum
-from ordered_enum.ordered_enum import ValueOrderedEnum
-
-__version__ = "0.0.8"
