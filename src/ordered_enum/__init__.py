992af9540c2283a4bc7e1dbe1146a371f0d6e7c5
sasa cocic <cocicsasa@gmail.com>
2024-06-05 21:38:24 -0400

    pass ci lint

--- src/ordered_enum/__init__.py
+++ src/ordered_enum/__init__.py
@@ -1,5 +1,4 @@
-from ordered_enum.ordered_enum import OrderedEnum
-from ordered_enum.ordered_enum import ValueOrderedEnum
+from ordered_enum.ordered_enum import OrderedEnum, ValueOrderedEnum
 
 __version__ = "0.0.8"
 
