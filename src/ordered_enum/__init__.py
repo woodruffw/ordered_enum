f2514974448a31717f6af144044e8dfaadf680b4
Sasa Cocic-Banjanac <cocicsasa@gmail.com>
2024-06-05 21:43:25 -0400

    Fix: make `OrderedEnum` and `ValueOrderedEnum` considered public (#29)
    * change exports to be public
    
    * change export format
    
    * change order of exports
    
    * move __all__ to end
    
    * pass ci lint

--- src/ordered_enum/__init__.py
+++ src/ordered_enum/__init__.py
@@ -1,3 +1,9 @@
-from ordered_enum.ordered_enum import OrderedEnum, ValueOrderedEnum  # noqa: F401
+from ordered_enum.ordered_enum import OrderedEnum, ValueOrderedEnum
 
 __version__ = "0.0.8"
+
+__all__ = [
+    "__version__",
+    "OrderedEnum",
+    "ValueOrderedEnum",
+]
